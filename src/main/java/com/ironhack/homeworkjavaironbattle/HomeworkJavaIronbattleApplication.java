6c1da1626dbbbd197ea7f4e06e9512dfb81783b5
shaig-mahmudov <zyphorix.official@gmail.com>
2026-03-06 02:28:26 -0800

    first commit

---
+++ src/main/java/com/ironhack/homeworkjavaironbattle/HomeworkJavaIronbattleApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.homeworkjavaironbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkJavaIronbattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkJavaIronbattleApplication.class, args);
+    }
+
+}
